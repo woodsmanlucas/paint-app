e799439b69c8da42b9055ca781c42770baf541a0
Lucas Johnson <woodsman.lucas@gmail.com>
2020-02-28 10:56:03 -0800

    added login page

--- paint/src/App.js
+++ paint/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Navbar } from './components/Navbar'
+import { Login } from './components/Login'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
         <Navbar />
-    </div>
+
+        <Switch>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        </Switch>
+    </Router>
   );
 }
 
